1bdc6f830796793d4d3765e567178791ae6f6acf
ShimizuShiori <116971263@qq.com>
2018-02-13 19:33:16 +0800

    创建编辑器

--- scripts/require.main.js
+++ scripts/require.main.js
@@ -2,6 +2,12 @@ require.config({
     paths: {
         reqwest: "https://cdn.bootcss.com/reqwest/2.0.5/reqwest.min",
         jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min",
-        markdown: "https://cdn.bootcss.com/markdown.js/0.6.0-beta1/markdown.min"
+        markdown: "https://cdn.bootcss.com/markdown.js/0.6.0-beta1/markdown.min",
+        vue: "https://cdn.bootcss.com/vue/2.5.13/vue.min.js"
+    },
+    shim: {
+        vue: {
+            exports: "Vue"
+        }
     }
 });
