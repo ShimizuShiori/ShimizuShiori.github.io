7d8ea07278ad2c530df1f6388f8e2b26fd3485e4
ShimizuShiori <116971263@qq.com>
2018-02-13 11:51:52 +0800

    1

--- scripts/require.main.js
+++ scripts/require.main.js
@@ -1,5 +1,6 @@
 require.config({
     paths: {
-        reqwest: "https://cdn.bootcss.com/reqwest/2.0.5/reqwest.min"
+        reqwest: "https://cdn.bootcss.com/reqwest/2.0.5/reqwest.min",
+        jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min"
     }
 });
