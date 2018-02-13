833eca094c973d038a4dc6b771a32476dc0bfbd4
ShimizuShiori <116971263@qq.com>
2018-02-13 14:08:28 +0800

    join markdown

--- scripts/require.main.js
+++ scripts/require.main.js
@@ -1,6 +1,7 @@
 require.config({
     paths: {
         reqwest: "https://cdn.bootcss.com/reqwest/2.0.5/reqwest.min",
-        jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min"
+        jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min",
+        markdown: "https://cdn.bootcss.com/markdown.js/0.6.0-beta1/markdown.min"
     }
 });
