75ad0fb97d1d5301e8db61fcc291c4dfc0621954
ShimizuShiori <116971263@qq.com>
2018-02-22 15:18:26 +0800

    create art about git

--- scripts/require.main.js
+++ scripts/require.main.js
@@ -2,6 +2,8 @@ require.config({
     paths: {
         reqwest: "https://cdn.bootcss.com/reqwest/2.0.5/reqwest.min",
         jquery: "https://cdn.bootcss.com/jquery/3.3.1/jquery.min",
-        markdown: "https://cdn.bootcss.com/markdown.js/0.6.0-beta1/markdown.min"
+        marked: "https://cdn.bootcss.com/marked/0.3.12/marked.min"
+    },
+    sim: {
     }
 });
